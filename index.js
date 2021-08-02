d8071b64a345a6274ab768942ab08d5b85afa7f7
sharif-islam <abmsharif555@gmaile.com>
2021-08-03 02:52:37 +0600

    server-code

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwvev.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("orders");
+
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    console.log(product);
+    productsCollection.insertOne(product).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.get("/products", (req, res) => {
+    productsCollection
+      .find({})
+      .limit(20)
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.get("/product/:key", (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  app.post("/productsByKeys", (req, res) => {
+    const productKeys = req.body;
+    productsCollection
+      .find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    console.log(order);
+    ordersCollection.insertOne(order).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
